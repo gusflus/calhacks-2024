717e66f6c7f899eac9e9b570be15a6854d6db2e0
gusflus <gus.flusser@gmail.com>
2024-06-22 17:52:50 -0700

    blam

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,11 +3,27 @@ import Footer from "../components/Footer.tsx";
 import Navbar from "../components/Navbar.tsx";
 
 const Login = () => {
+  const providers = ["Google", "Facebook", "Twitter"];
+
   return (
-    <div>
+    <div className="">
+      <Navbar />
       <div className="login">
-        <Navbar />
-        <h2 className="title">login page</h2>
+        <div>
+          <h2 className="title">
+            Login with your
+            <br />
+            Medical Provider
+          </h2>
+        </div>
+        <div>
+          <p>Please select a provider to log in with:</p>
+          <ul>
+            {providers.map((provider) => (
+              <li key={provider}>{provider}</li>
+            ))}
+          </ul>
+        </div>
       </div>
       <Footer />
     </div>
