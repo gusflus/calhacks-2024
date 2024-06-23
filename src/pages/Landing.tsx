0016f4aaddf0c78ac4371b2124c6a5ff457f6a9d
Blake <kanssdev@gmail.com>
2024-06-23 00:20:50 -0700

    git

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -3,14 +3,20 @@ import Footer from "../components/Footer.tsx";
 import Navbar from "../components/Navbar.tsx";
 import "./Landing.css";
 
-const Landing = ({ title, subtitle }: { title: string; subtitle: string }) => {
+const Landing = () => {
   return (
     <div className="landing">
       <Navbar />
       <main className="content">
-        <div className="centerBox">
-          <h1>{title}</h1>
-          <p>{subtitle}</p>
+        <div className="infoBox">
+          <h1>World-class preventive and specialty care</h1>
+          <p>
+            Learn how your primary and specialty care teams work together at every step — leading to better prevention, better treatment, and better outcomes.
+          </p>
+          <button className="learnMore">Learn more</button>
+        </div>
+        <div className="imageBox">
+          <img src={require("../assets/outofcollegemoneyspentseenofuturepaynorent_allthemoneysgonenowheretogo.jpg")} alt="Medical professionals looking at screens" />
         </div>
       </main>
       <Footer />
