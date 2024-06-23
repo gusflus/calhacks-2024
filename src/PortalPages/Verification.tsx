5c29def8436c9a729e71b4bc93743710a841a3da
gusflus <gus.flusser@gmail.com>
2024-06-22 21:16:07 -0700

    pages

---
+++ src/PortalPages/Verification.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./PortalPage.css";
+
+const Verification = () => {
+  return (
+    <div>
+      <h1>Verification</h1>
+    </div>
+  );
+};
+
+export default Verification;
--- src/pages/Portal.tsx
+++ src/pages/Portal.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "../components/Footer.tsx";
 import Navbar from "../components/Navbar.tsx";
 import "./Portal.css";
 
+import Verification from "../PortalPages/Verification.tsx";
+
 const Portal = () => {
+  const [currentPage, setCurrentPage] = useState<string>("section1");
   const navItems = [
     { id: "section1", title: "Your Scan" },
     { id: "section2", title: "Impact Areas" },
@@ -17,6 +20,31 @@ const Portal = () => {
     }
   };
 
+  const getCurrentPage = () => {
+    switch (currentPage) {
+      case "section1":
+        return <Verification />;
+      case "section2":
+        return (
+          <section id="section2">
+            <h2>Impact Areas</h2>
+            <p>This is the content for section 2.</p>
+            <img src="https://via.placeholder.com/400x200" alt="Placeholder" />
+          </section>
+        );
+      case "section3":
+        return (
+          <section id="section3">
+            <h2>Section 3</h2>
+            <p>This is the content for section 3.</p>
+            <img src="https://via.placeholder.com/400x200" alt="Placeholder" />
+          </section>
+        );
+      default:
+        return <Verification />;
+    }
+  };
+
   return (
     <div className="portal">
       <Navbar logout />
@@ -26,10 +54,7 @@ const Portal = () => {
             <ul>
               {navItems.map((item) => (
                 <li key={item.id}>
-                  <div
-                    onClick={() => scrollToSection(item.id)}
-                    className="item"
-                  >
+                  <div onClick={() => setCurrentPage(item.id)} className="item">
                     {item.title}
                   </div>
                 </li>
@@ -37,32 +62,7 @@ const Portal = () => {
             </ul>
           </nav>
         </aside>
-        <main className="main-content">
-          <h1>Your Portal</h1>
-          <section id="section1">
-            <h2>Your Scan</h2>
-            <p>
-              Below is your results from your most recent MRI scan. Please
-              review the results and reach out to your provider with any
-              questions.
-            </p>
-            <img src="https://via.placeholder.com/400x200" alt="Placeholder" />
-          </section>
-          <section id="section2">
-            <h2>Impact Areas</h2>
-            <p>This is the content for section 2.</p>
-            <img src="https://via.placeholder.com/400x200" alt="Placeholder" />
-          </section>
-          <section id="section3">
-            <h2>Section 3</h2>
-            <p>This is the content for section 3.</p>
-            <img src="https://via.placeholder.com/400x200" alt="Placeholder" />
-          </section>
-          <section id="carousel">
-            <h2>Carousel</h2>
-            <p>Your carousel component will go here.</p>
-          </section>
-        </main>
+        <main className="main-content">{getCurrentPage()}</main>
       </div>
       <Footer />
     </div>
